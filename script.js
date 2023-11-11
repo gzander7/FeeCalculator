5ce591c79a8b01b4dec05ea7484ec2847cd79435
gzander7 <79114634+gzander7@users.noreply.github.com>
2023-11-10 23:14:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function calculateAgeMultiplier(age) {
     if (0 <= age && age <= 1) {
-        return 2.4;
+        return 2.40;
     } else if (1 < age && age <= 2) {
         return 2.36;
     } else if (2 < age && age <= 3) {
@@ -10,7 +10,7 @@ function calculateAgeMultiplier(age) {
     } else if (4 < age && age <= 5) {
         return 2.24;
     } else if (5 < age && age <= 6) {
-        return 2.2;
+        return 2.20;
     } else if (6 < age && age <= 7) {
         return 2.16;
     } else if (7 < age && age <= 8) {
@@ -52,91 +52,53 @@ function calculateCost() {
 }
 
 function reset() {
+    // Reset input values
     document.getElementById('length').value = '';
     document.getElementById('width').value = '';
     document.getElementById('age').value = '';
+
+    // Reset result display
     document.getElementById('result').innerText = '';
+
+    // Clear advanced calculation details
+    var detailsElement = document.getElementById('calculationDetails');
+    if (detailsElement) {
+        detailsElement.remove();
+    }
+
+    // Uncheck the advanced calculation checkbox
+    document.getElementById('showAdvancedCalc').checked = false;
+
+    // Clear street age multiplier display
+    document.getElementById('streetAgeDisplay').innerText = '';
 }
 
 function showAdvancedCalc() {
-    var length = parseFloat(document.getElementById('length').value);
-    var width = parseFloat(document.getElementById('width').value);
-    var age = parseFloat(document.getElementById('age').value);
-    var ageMultiplier = 0;
-
-    if (isNaN(length) || isNaN(width) || isNaN(age)) {
-        console.log('Invalid input values');
-        return;
-    }
-    var calculationDetails = '';
-
-    if (0 <= age && age <= 1) {
-        ageMultiplier = 2.4;
-        calculationDetails += `Cost For Age: $2.40\n`;
-    } else if (1 < age && age <= 2) {
-        ageMultiplier = 2.36;
-        calculationDetails += `Cost For Age: $2.36\n`;
-    } else if (2 < age && age <= 3) {
-        ageMultiplier = 2.32;
-        calculationDetails += `Cost For Age: $2.32\n`;
-    } else if (3 < age && age <= 4) {
-        ageMultiplier = 2.28;
-        calculationDetails += `Cost For Age: $2.28\n`;
-    } else if (4 < age && age <= 5) {
-        ageMultiplier = 2.24;
-        calculationDetails += `Cost For Age: $2.24\n`;
-    } else if (5 < age && age <= 6) {
-        ageMultiplier = 2.2;
-        calculationDetails += `Cost For Age: $2.2\n`;
-    } else if (6 < age && age <= 7) {
-        ageMultiplier = 2.16;
-        calculationDetails += `Cost For Age: $2.16\n`;
-    } else if (7 < age && age <= 8) {
-        ageMultiplier = 2.12;
-        calculationDetails += `Cost For Age: $2.12\n`;
-    } else if (8 < age && age <= 9) {
-        ageMultiplier = 2.08;
-        calculationDetails += `Cost For Age: $2.08\n`;
-    } else if (9 < age && age <= 10) {
-        ageMultiplier = 2.04;
-        calculationDetails += `Cost For Age: $2.04\n`;
-    } else if (age > 10) {
-        ageMultiplier = 2;
-        calculationDetails += `Cost For Age: $2.00\n`;
-    }
-
-    // Intermediate calculations
-    var cutLength = length;
-    var cutWidth = width;
-    var areaOfInfluenceLength = cutLength + 4;
-    var areaOfInfluenceWidth = cutWidth + 4;
-    var areaOfInfluenceSqFeet = areaOfInfluenceLength * areaOfInfluenceWidth;
-    var cost = ageMultiplier * areaOfInfluenceSqFeet
-    cost = cost.toFixed(2);
-
-    calculationDetails += `Cut Length: ${cutLength}\n`;
-    calculationDetails += `Cut Width: ${cutWidth}\n`;
-    calculationDetails += `Area Of Influence Length: ${areaOfInfluenceLength}\n`;
-    calculationDetails += `Area Of Influence Width: ${areaOfInfluenceWidth}\n`;
-    calculationDetails += `Area Of Influence Sq Feet: ${areaOfInfluenceSqFeet}\n`;
-    calculationDetails += `${areaOfInfluenceSqFeet} Sq ft * $${ageMultiplier} = $${cost}`;
-
-    // Display the calculation details under the result
-    var resultElement = document.getElementById('result');
-    var detailsElement = document.getElementById('calculationDetails');
     var showDetailsCheckbox = document.getElementById('showAdvancedCalc');
+    var detailsElement = document.getElementById('calculationDetails');
 
-    // Check if the checkbox is checked
+    // If the checkbox is checked, show the details
     if (showDetailsCheckbox.checked) {
+        var age = parseFloat(document.getElementById('age').value);
+        var ageMultiplier = calculateAgeMultiplier(age);
+        var length = parseFloat(document.getElementById('length').value);
+        var width = parseFloat(document.getElementById('width').value);
+
         // If the details element doesn't exist, create it and append it
         if (!detailsElement) {
             detailsElement = document.createElement('div');
             detailsElement.id = 'calculationDetails';
+            var resultElement = document.getElementById('result');
             resultElement.insertAdjacentElement('afterend', detailsElement);
         }
 
         // Set the content of the details element
-        detailsElement.innerText = calculationDetails;
+        detailsElement.innerText = `Age Multiplier: $${ageMultiplier.toFixed(2)}`;
+        detailsElement.innerText += `\nCut Length: ${isNaN(length) ? 0 : length} ft`;
+        detailsElement.innerText += `\nCut Width: ${isNaN(width) ? 0 : width} ft`;
+        detailsElement.innerText += `\nArea Of Infulecnce: ${isNaN(length) ? 0 : length + 4} ft^2`;
+        detailsElement.innerText += `\nArea of Infulence: ${isNaN(width) ? 0 :width + 4} ft^2`;
+        detailsElement.innerText += `\nArea of Infulence: ${isNaN(length) || isNaN(width) ? 0 :(length + 4) * (width + 4)} ft^2`;
     } else {
         // If the checkbox is unchecked, remove the details element if it exists
         if (detailsElement) {
@@ -145,39 +107,39 @@ function showAdvancedCalc() {
     }
 }
 
-
-
 function displayStreetAgeMultiplyer() {
     var streetAgeInput = document.getElementById('age');
     var streetAgeDisplay = document.getElementById('streetAgeDisplay');
-    
-    // Check if the age input is a valid number
-    var age = parseFloat(streetAgeInput.value);
-    if (!isNaN(age)) {
-        // Calculate the age multiplier and display it
-        var ageMultiplier = calculateAgeMultiplier(age);
-        streetAgeDisplay.innerText = `Cost for age is $${ageMultiplier.toFixed(2)}`;
+
+    // Trim leading and trailing whitespaces
+    var trimmedValue = streetAgeInput.value.trim();
+
+    // Check if there is a value in the street age input
+    if (trimmedValue !== '') {
+        streetAgeDisplay.innerHTML = '$' + (calculateAgeMultiplier(trimmedValue)).toFixed(2);
     } else {
-        // Handle invalid input
-        streetAgeDisplay.innerText = '';
+        // If no value, display nothing or any default message you prefer
+        streetAgeDisplay.innerHTML = '';
     }
 }
 
+
+window.onload = displayStreetAgeMultiplyer;
 
 document.getElementById('age').addEventListener('input', showAdvancedCalc);
 document.getElementById('length').addEventListener('input', showAdvancedCalc);
 document.getElementById('width').addEventListener('input', showAdvancedCalc);
 document.getElementById('age').addEventListener('input', function () {
-    calculateCost(); // Call calculateCost whenever 'age' input changes
+    calculateCost(); 
     showAdvancedCalc();
 });
 
 document.getElementById('length').addEventListener('input', function () {
-    calculateCost(); // Call calculateCost whenever 'length' input changes
+    calculateCost(); 
     showAdvancedCalc();
 });
 
 document.getElementById('width').addEventListener('input', function () {
-    calculateCost(); // Call calculateCost whenever 'width' input changes
+    calculateCost(); 
     showAdvancedCalc();
 });
